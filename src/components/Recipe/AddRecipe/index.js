b94a5d6ed936b22f57da029b297e7edf0b3f7abb
Brittany Crocker <brittany.crocker@metigy.com>
2021-10-16 15:39:15 +1100

    value and onchange handler added to addrecipe inputs

--- src/components/Recipe/AddRecipe/index.js
+++ src/components/Recipe/AddRecipe/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Layout, Menu, Breadcrumb, Dropdown, Button, message, Typography , Input } from 'antd';
 import { Container, TitleInput, ContentContainer, InputContainer, Section } from './index.styles'
 import { EditOutlined, UserOutlined, DownOutlined, } from '@ant-design/icons'
@@ -7,6 +7,11 @@ const { TextArea } = Input
 const { Title } = Typography
 
 const AddRecipeContent = () => {
+    const [serves, setServes] = useState()
+    const [ingredients, setIngredients] = useState()
+    const [cookingTime, setCookingTime] = useState()
+    const [mealType, setMealType] = useState()
+    const [directions, setDirections] = useState()
 
     function handleButtonClick(e) {
         message.info('Click on left button.');
@@ -14,6 +19,7 @@ const AddRecipeContent = () => {
       }
       
       function handleMenuClick(e) {
+          setMealType(e.target.value)
         message.info('Click on menu item.');
         console.log('click', e);
       }
@@ -42,12 +48,16 @@ const AddRecipeContent = () => {
                     <Input
                         prefix={<UserOutlined className="site-form-item-icon" />}
                         placeholder='serves'
+                        value={serves}
+                        onChange={(e) => setServes(e.target.value)}
                     />
                     </InputContainer>
                     <InputContainer>
                         <Input
                         prefix={<UserOutlined className="site-form-item-icon" />}
                         placeholder='cooking time'
+                        value={cookingTime}
+                        onChange={(e) => setCookingTime(e.target.value)}
                         />
                         </InputContainer>
                     <InputContainer>
@@ -57,14 +67,21 @@ const AddRecipeContent = () => {
                         </Dropdown.Button>
                     </InputContainer>
                     <InputContainer>
-                    <Title level={4}>Ingredients List</Title>
-                        <TextArea rows={11} />
+                    <Title 
+                        level={4}>
+                            Ingredients List
+                        </Title>
+                        <TextArea 
+                        rows={11} 
+                        value={ingredients}
+                        onChange={(e) => setIngredients(e.target.value)}/>
                     </InputContainer>
                 </Section>
                 <Section>
                     <InputContainer style={{}}><Title level={4}>Directions</Title></InputContainer>
                     <InputContainer>
-                        <TextArea rows={16} />
+                        <TextArea rows={16} value={directions}
+                        onChange={(e) => setDirections(e.target.value)}/>
                     </InputContainer> 
                 </Section>
             </ContentContainer>
